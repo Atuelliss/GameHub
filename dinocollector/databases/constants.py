e938f40bf03ebf314b35482e205c633de86655d7
Vainne <148051936+Atuelliss@users.noreply.github.com>
2025-12-14 11:20:20 -0500

    Revise DinoCoin values and disallowed names
    Updated DinoCoin values for creatures and modified disallowed names.

--- dinocollector/databases/constants.py
+++ dinocollector/databases/constants.py
@@ -1,17 +1,17 @@
 # DinoCoin value for creatures in creature_library
-common_value = [1, 5]
-uncommon_value = [10, 15]
-semi_rare_value = [20, 30]
-rare_value = [30, 40]
-very_rare_value = [45, 60]
-super_rare_value = [65, 80]
+common_value = [10, 25]
+uncommon_value = [25, 45]
+semi_rare_value = [40, 65]
+rare_value = [50, 70]
+very_rare_value = [65, 85]
+super_rare_value = [80, 100]
 legendary_value = [90, 120]
 event_value = [150, 175]
 
 DEFAULT_DISALLOWED_NAMES = [
-    "admin", "moderator", "owner", "bot", "system", "null", 
-    "nigger", "nigga", "kook", "gook", "master", "slave", 
-    "bitch", "slut", "whore", "ho", "hoe", "cunt", 
-    "cock", "schlong", "rape", "dick", "dickhead", "penis", 
+    "admin", "moderator", "owner", "system", "null", 
+    "nigger", "nigga", "kook", "gook", "slave", "bitch", 
+    "slut", "whore", "cunt", "cock", "schlong", "rape", 
+    "dick", "dickhead", "penis", 
     "vagina", "pussy", "twat"
 ]
