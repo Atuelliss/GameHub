13d4dbf1e79c2e9e54e5b5839573c57d08217d8f
Vainne <148051936+Atuelliss@users.noreply.github.com>
2025-12-28 16:23:17 -0500

    Revise DinoCoin values and disallowed names
    Updated DinoCoin values for various creature rarities and modified the disallowed names list.

--- dinocollector/databases/constants.py
+++ dinocollector/databases/constants.py
@@ -1,12 +1,12 @@
 # DinoCoin value for creatures in creature_library
-common_value = [10, 25]
-uncommon_value = [25, 45]
-semi_rare_value = [40, 65]
-rare_value = [50, 70]
-very_rare_value = [65, 85]
-super_rare_value = [80, 100]
-legendary_value = [90, 120]
-event_value = [150, 175]
+common_value = [10, 35]
+uncommon_value = [25, 65]
+semi_rare_value = [40, 95]
+rare_value = [50, 115]
+very_rare_value = [65, 130]
+super_rare_value = [80, 150]
+legendary_value = [90, 165]
+event_value = [150, 215]
 
 DEFAULT_DISALLOWED_NAMES = [
     "admin", "moderator", "owner", "system", "null", 
