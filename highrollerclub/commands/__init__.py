920e393da50e4532552626fb8e5dc43b170dddbe
Vainne <jrdear@gmail.com>
2026-09-24 00:42:17 -0400

    Removed highroller from repo

--- highrollerclub/commands/__init__.py
+++
@@ -1,7 +0,0 @@
-from ..abc import CompositeMetaClass
-from .admin_commands import Admin
-from .user_commands import UserCommands
-
-
-class Commands(Admin, UserCommands, metaclass=CompositeMetaClass):
-    """Subclass all command classes"""
